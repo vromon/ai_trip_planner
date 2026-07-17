cd2a6eedfb536791c2d9c9d725c6999eb7474414
Rahat Ahmed Khan <rahatofficial.29@gmail.com>
2026-07-17 12:13:26 +0600

    refactor: improve global exception handling

--- app/middlewares/exception_handler.py
+++ app/middlewares/exception_handler.py
@@ -3,10 +3,11 @@ from fastapi.responses import JSONResponse
 def register_exception_handlers(app:FastAPI):
     @app.exception_handler(Exception)
     def global_exception_handler(request:Request,exc:Exception):
+        print(f"server error:{exc}")
         return JSONResponse(
             status_code=500,
             content={
                 "success":False,
-                "message":"Internel Server Error"
+                "message":"Internal Server Error"
             }
         )
