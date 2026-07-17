f266f27c2cbec63fd1788310a2e00126d945de0b
Rahat Ahmed Khan <rahatofficial.29@gmail.com>
2026-07-17 12:09:00 +0600

    chore: update environment configuration

--- app/config.py
+++ app/config.py
@@ -1,4 +1,7 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     model_config=SettingsConfigDict(env_file=".env",extra="ignore")
+    PINECONE_API:str
+    GEMINI_API:str
+    TRIP_INDEX:str
 settings=Settings()
